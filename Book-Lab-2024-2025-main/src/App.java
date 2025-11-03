0c8219877869b3b570aa33ff44d6ad3d574a2956
ziyad-ash11 <za7258@pleasantonusd.net>
2025-11-03 10:31:54 -0800

    Add files via upload

---
+++ Book-Lab-2024-2025-main/src/App.java
@@ -0,0 +1,7 @@
+class App {
+  public static void main(String[] args) {
+    Book aBook = new Book();
+    System.out.println(aBook.pigLatin("hello"));
+    System.out.println(aBook.translateWord("hello"));
+  }
+}
---
+++ Book-Lab-2024-2025-main/src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+    public String pigLatin(String word) {
+        String newString = "";
+        String vowel = "aeiou";
+        String numbers = "0123456789";
+        if (word.length()==0) {
+            return word;
+        }
+        else if (numbers.indexOf(word.substring(0,1)) >= 0) {
+            return word + "ay";
+        }
+        else if (vowel.indexOf(word.substring(0,1)) >= 0) {
+            return word + "yay";
+        }
+        for (int i = 0; i<word.length();i++) {
+            if (vowel.indexOf(word.substring(i,i+1)) >= 0) {
+                return word.substring(i,word.length()) + word.substring(0,i) + "ay";
+            }
+        }
+
+        return word;
+
+
+    }
+    public int endPunctuation(String word) {
+        return -1;
+    }
+    public String translateWord(String word) {
+        String convertedWord = "";
+        return convertedWord;
+    }
+    public String translateSentence(String setence) {
+        String convertedSentence = "";
+        return convertedSentence;
+    }
+}
